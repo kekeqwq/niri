7ab56edb2ad58364d3c22ba177e6f6039664be2c
keke <zrignshw@duck.com>
2025-10-29 18:59:09 +0800

    .

--- rofi/power.sh
+++ rofi/power.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e "🔌 Poweroff\n♻️ Reboot\n🌙 Suspend\n🔒 Lock (INOP)\n🏃‍♀️ Logout" | rofi -i -dmenu | awk '{print tolower($2)}' )
+op=$( echo -e "🔌 Poweroff\n♻️ Reboot\n🌙 Suspend\n🔒 Lock (INOP)\n🏃‍♀️ Logout\n🖥 Close" | rofi -i -dmenu | awk '{print tolower($2)}' )
 
 case $op in 
     poweroff)
@@ -16,4 +16,7 @@ case $op in
     logout)
         niri msg action quit --skip-confirmation
         ;;
+    close)
+	niri msg action power-off-monitors
+	;;
 esac
